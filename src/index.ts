a2b4e9fadcddd17e26218c507cafb061b44de397
Logan Barsell <loganjbars@gmail.com>
2022-05-04 11:20:42 -0700

    json server + fetch/save

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import { User } from '../models/Users';
+import { User } from './models/Users';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.on('change', () => {
-  console.log('changed');
-});
-
-user.on('change', () => {
-  console.log('changed again');
-});
-
-user.on('fuck', () => {
-  console.log('fucked');
-});
-
-user.trigger('change');
-user.trigger('fuck');
-
-console.log(user);
+user.save();
